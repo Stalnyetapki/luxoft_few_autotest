bf9b67f21352b6826bb15512dc65cd4bf88c40d3
Vasenkov Stanislav <aanher@gmail.com>
2022-02-04 20:39:12 +0300

    Update GeneratedTests.java

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -5,7 +5,7 @@ import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
